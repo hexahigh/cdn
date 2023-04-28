0ea63c2d7f3a02b27f7e974fab7d422db1ba1efb
Boof <97455552+hexahigh@users.noreply.github.com>
2023-04-28 11:39:02 +0200

    uh

--- other/torrent-create.mjs
+++ other/torrent-create.mjs
@@ -1,7 +1,9 @@
 import createTorrent from 'create-torrent'
 import fs from 'fs'
 
-createTorrent('./', [urlList: "https://boof.eu.org",] (err, torrent) => {
+var opts = ""
+
+createTorrent('./', [opts], (err, torrent) => {
   if (!err) {
     // `torrent` is a Buffer with the contents of the new .torrent file
     fs.writeFile('site.torrent', torrent)
